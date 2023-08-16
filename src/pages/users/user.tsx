95db63d5929744bff19c850a69be21b2d6824bc4
SmartSqube <SmartSqube@gmail.com>
2023-08-16 18:38:33 +0500

    user

--- src/pages/users/user.tsx
+++ src/pages/users/user.tsx
@@ -2,5 +2,9 @@ import { FC } from "react";
 import './user.css';
 
 export const User:FC = ()=>{
-    return 
+    return(
+        <div>
+            <button>Play</button>
+        </div>
+    )
 }
