86e2bd284c4a2f06a625e6c8a43a55b3e449d36e
SmartSqube <smartSqube@gmail.com>
2023-08-20 11:41:17 +0500

    [school-3]: fix review

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { GlobalStyles } from "./app.style";
+import { GlobalStyles } from "./app.styles";
 import { NavigationLayout } from "./layouts";
 
 function App() {
-  const activeView: string = "page2";
   return (
     <>
       <GlobalStyles />
-      {activeView === "page1" && (
-        <NavigationLayout>Гимис колбис</NavigationLayout>
-      )}
-      {activeView === "page2" && (
-        <NavigationLayout>Другой контент</NavigationLayout>
-      )}
+      <NavigationLayout>Гимис колбис</NavigationLayout>
     </>
   );
 }
